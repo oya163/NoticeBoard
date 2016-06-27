0a5108a4ca97235eea574b3de8dae00b964908ec
oya163 <oyeshsin@hotmail.com>
2016-06-27 03:21:13 -0500

    Login related modifications

--- SourceFiles/mainwindow.cpp
+++ SourceFiles/mainwindow.cpp
@@ -1,14 +1,67 @@
 #include "HeaderFiles/mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //UsersDBManager();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+QString MainWindow::getUserName(){
+    return MainWindow::username;
+}
+
+QString MainWindow::getPassword(){
+    return MainWindow::password;
+}
+
+void MainWindow::setUserName(QString inUserName){
+    MainWindow::username = inUserName;
+}
+
+void MainWindow::setPassword(QString inPassword){
+    MainWindow::password = inPassword;
+}
+
+void MainWindow::on_loginButon_clicked()
+{
+    UsersDBManager udb;
+
+    //Checking the input username and password are give or not
+    if(ui->userName_txt->text().isEmpty() || ui->password_txt->text().isEmpty()){
+        mainMsgBox.setText("Please input the username and password");
+        mainMsgBox.exec();
+    }
+    setUserName(ui->userName_txt->text());
+    setPassword(ui->password_txt->text());
+
+    bool successLogin = udb.checkLogin(getUserName(),getPassword());
+
+    if(successLogin){
+        //display another home window
+
+    }
+    else
+    {
+        //display qmessage login failed
+        mainMsgBox.setText("Login failed. Please enter correct \nusername and password");
+        mainMsgBox.exec();
+    }
+
+}
+
+
+
+void MainWindow::on_exitButton_clicked()
+{
+    QApplication::quit();
+
 }
--- SourceFiles/usersdbmanager.cpp
+++ SourceFiles/usersdbmanager.cpp
@@ -1,6 +1,38 @@
-#include "usersdbmanager.h"
+#include "HeaderFiles/usersdbmanager.h"
+#include <algorithm>
 
 UsersDBManager::UsersDBManager()
 {
+    users_db = QSqlDatabase::addDatabase("QSQLITE");
+    QString path = "/home/oyashi/Qt_Project/NoticeBoard/Databases/NoticeBoard.db";
+    users_db.setDatabaseName(path);
 
+    if (!users_db.open())
+    {
+       qDebug() << "Error: connection with database fail";
+    }
+    else
+    {
+       qDebug() << "Database: connection ok";
+    }
 }
+
+//Check the login credentials are correct or not
+bool UsersDBManager::checkLogin(QString uname, QString pwd){
+    //For unit testing purpose
+    qDebug() << uname << " " << pwd << endl;
+
+    QSqlQuery checkQuery;
+    checkQuery.prepare("SELECT COUNT(*) FROM users WHERE USERNAME = (:uname) AND PASSWORD = (:pwd)");
+    checkQuery.bindValue(":uname",uname);
+    checkQuery.bindValue(":pwd",pwd);
+    checkQuery.exec();
+
+    if(checkQuery.next()){
+        if(checkQuery.value(0).toInt() == 1){
+            return true;
+        }
+    }
+    return false;
+}
+
