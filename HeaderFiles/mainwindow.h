fe701d53dd3ce75bb66280474d4857cecebe9aa2
oya163 <oyeshsin@hotmail.com>
2016-06-27 03:18:45 -0500

    Initial mainwindow file

---
+++ HeaderFiles/mainwindow.h
@@ -0,0 +1,45 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMessageBox>
+
+#include "HeaderFiles/usersdbmanager.h"
+
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+    QString getUserName();
+    void setUserName(QString inUserName);
+
+    QString getPassword();
+    void setPassword(QString inPassword);
+
+private slots:
+    void on_loginButon_clicked();
+
+    void on_exitButton_clicked();
+
+
+private:
+    Ui::MainWindow *ui;
+    QMessageBox mainMsgBox;
+
+    QString username;
+    QString password;
+
+
+
+};
+
+#endif // MAINWINDOW_H
