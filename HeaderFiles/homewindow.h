4a8047013648dbbc5db81a3fb508924765eacc49
oya163 <oyeshsin@hotmail.com>
2016-07-01 01:01:44 -0500

    Activated create button

--- HeaderFiles/homewindow.h
+++ HeaderFiles/homewindow.h
@@ -15,6 +15,7 @@ public:
     explicit HomeWindow(QWidget *parent = 0);
     virtual ~HomeWindow();
 
+
 public slots:
     void setWelcomeLabel(QString uName);
 
@@ -22,12 +23,14 @@ public slots:
 private slots:
     void on_logout_button_clicked();
 
+    void on_createButton_clicked();
+
 private:
     Ui::HomeWindow *uih;
 
     QString userName;
-};
 
+};
 
 
 #endif // HOMEWINDOW_H
--- SourceFiles/homewindow.cpp
+++ SourceFiles/homewindow.cpp
@@ -1,13 +1,12 @@
-#include "HeaderFiles/mainwindow.h"
 #include "HeaderFiles/homewindow.h"
 #include "ui_homewindow.h"
+#include "HeaderFiles/createwindow.h"
 
 HomeWindow::HomeWindow(QWidget *parent) :
     QMainWindow(parent),
     uih(new Ui::HomeWindow)
 {
     uih->setupUi(this);
-
 }
 
 HomeWindow::~HomeWindow()
@@ -17,10 +16,18 @@ HomeWindow::~HomeWindow()
 
 void HomeWindow::on_logout_button_clicked()
 {
+    MainWindow *mw = new MainWindow();
     this->close();
+    mw->show();
 }
 
 void HomeWindow::setWelcomeLabel(QString uName){
     uih->wcName->setText(uName);
 }
 
+
+void HomeWindow::on_createButton_clicked()
+{
+    CreateWindow *create = new CreateWindow();
+    create->show();
+}
