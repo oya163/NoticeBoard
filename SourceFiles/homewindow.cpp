a989615bdf4c19a415d85f40649d5f07dacc117f
oya163 <oyeshsin@hotmail.com>
2016-07-01 06:26:22 -0500

    Completed display of current month notice on TableView

--- SourceFiles/homewindow.cpp
+++ SourceFiles/homewindow.cpp
@@ -1,12 +1,15 @@
 #include "HeaderFiles/homewindow.h"
 #include "ui_homewindow.h"
 #include "HeaderFiles/createwindow.h"
+#include <QSqlTableModel>
 
 HomeWindow::HomeWindow(QWidget *parent) :
     QMainWindow(parent),
     uih(new Ui::HomeWindow)
 {
     uih->setupUi(this);
+
+    displayNotice();
 }
 
 HomeWindow::~HomeWindow()
@@ -31,4 +34,39 @@ void HomeWindow::on_createButton_clicked()
     QString un = userName;
     CreateWindow *create = new CreateWindow(0,un);
     create->show();
+}
+
+void HomeWindow::displayNotice(){
+    //Database connection
+    QSqlDatabase users_db = QSqlDatabase::addDatabase("QSQLITE");
+    QString path = "/home/oyashi/Qt_Project/NoticeBoard/Databases/NoticeBoard.db";
+    users_db.setDatabaseName(path);
+
+    if (!users_db.open())
+    {
+       qDebug() << "Error: connection with database fail";
+    }
+    else
+    {
+       qDebug() << "Database: connection ok FROM HOME WINDOW";
+    }
+
+    //Model Setup
+    model = new QSqlTableModel(uih->tableView);
+    model->setTable("NOTICE");
+    model->setEditStrategy(QSqlTableModel::OnManualSubmit);
+    //Filter to display current month NOTICE only
+    model->setFilter("strftime('%m',CREATEDON) = strftime('%m','now')");
+    model->select();
+    model->setHeaderData(0, Qt::Horizontal, tr("SN"));
+    model->setHeaderData(1, Qt::Horizontal, tr("For"));
+    model->setHeaderData(2, Qt::Horizontal, tr("Notice"));
+    model->setHeaderData(3, Qt::Horizontal, tr("From"));
+    model->setHeaderData(4, Qt::Horizontal, tr("Date"));
+    model->setHeaderData(5, Qt::Horizontal, tr("Read"));
+
+    uih->tableView->setModel(model);
+    uih->tableView->setSelectionMode(QAbstractItemView::SingleSelection);
+
+
 }
